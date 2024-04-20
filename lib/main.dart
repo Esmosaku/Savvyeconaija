c65fd6bf38d0e85b37df3b1120060a26f935c843
summydeveloper <adegbiteadekola54@gmail.com>
2024-04-20 14:40:22 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const SignupPage(),
+      home: const Splash(),
       debugShowCheckedModeBanner: false,
       routes: {
         'signupPage': (context) => const SignupPage(),
